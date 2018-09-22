6209985cb71d3dd7303dbc9f754731260b1f5947
nicorivero <nicolasnrivero@outlook.com>
2018-09-22 17:58:24 -0300

    Resolucion issue #1 Actualizacion de Parametros

--- api_tecn_emergentes/Controllers/SensoresController.cs
+++ api_tecn_emergentes/Controllers/SensoresController.cs
@@ -4,6 +4,7 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace api_tecn_emergentes.Controllers
 {
@@ -27,18 +28,45 @@ namespace api_tecn_emergentes.Controllers
         [HttpPut]
         public void SetTemperatura([FromBody] Parametros _dataToUpdate)
         {
+            //Obtener datos actuales de entidad
+            
+            var _param = JObject.Parse(Parametros(_dataToUpdate.id_entidad).ToString()).GetValue("sensores");
+            string ipAddress = _param.SelectToken("ip_sensor").ToString();
+            double tmax = double.Parse(_param.SelectToken("temp.max").ToString());
+            double tmin = double.Parse(_param.SelectToken("temp.min").ToString());
+            double hmax = double.Parse(_param.SelectToken("hum.max").ToString());
+            double hmin = double.Parse(_param.SelectToken("hum.min").ToString());
+
+            Sensor sensorUpdate = new Sensor() { 
+                ip_sensor = IPAddress.Parse(ipAddress),
+                temp = new Temperature() { min = _dataToUpdate.min, max = _dataToUpdate.max},
+                hum = new Humidity() { min = hmin, max = hmax} 
+                };
             //Revisar Metodo Update de clase DataAccess.
             IMongoCollection<BsonDocument> collection = data.GetCollection("Entidades");
-            data.UpdateDocument(collection, "id_entidad", _dataToUpdate.id_entidad, "sensores.temp", 
-                                new Temperature() { min = _dataToUpdate.min, max = _dataToUpdate.max } );
+            data.UpdateDocument(collection, "id_entidad", _dataToUpdate.id_entidad, "sensores", 
+                                sensorUpdate );
         }
 
         [HttpPut]
         public void SetHumedad([FromBody]Parametros _dataToUpdate)
         {
+            var _param = JObject.Parse(Parametros(_dataToUpdate.id_entidad).ToString()).GetValue("sensores");
+            string ipAddress = _param.SelectToken("ip_sensor").ToString();
+            double tmax = double.Parse(_param.SelectToken("temp.max").ToString());
+            double tmin = double.Parse(_param.SelectToken("temp.min").ToString());
+            double hmax = double.Parse(_param.SelectToken("hum.max").ToString());
+            double hmin = double.Parse(_param.SelectToken("hum.min").ToString());
+
+            Sensor sensorUpdate = new Sensor() { 
+                ip_sensor = IPAddress.Parse(ipAddress),
+                temp = new Temperature() { min = tmin, max = tmax},
+                hum = new Humidity() { min = _dataToUpdate.min, max = _dataToUpdate.max} 
+                };
+            //Revisar Metodo Update de clase DataAccess.
             IMongoCollection<BsonDocument> collection = data.GetCollection("Entidades");
-            data.UpdateDocument(collection, "id_entidad", _dataToUpdate.id_entidad, "hum", 
-                                new Humidity() { min = _dataToUpdate.min, max = _dataToUpdate.max });
+            data.UpdateDocument(collection, "id_entidad", _dataToUpdate.id_entidad, "sensores", 
+                                sensorUpdate );
         }
         
     }
